bae121a56ae4fcc7627fecbf8b7cea52ac3c8173
Maxmilián Fedorko <maxmilian.fedorko@evalue.cz>
2019-09-12 16:27:29 +0200

    working add form note

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../notes.model';
 import { DataService } from '../data.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
@@ -11,13 +12,27 @@ export class NotesComponent implements OnInit {
   notes: Note[];
   title: string;
   body: string;
+  addNoteForm: FormGroup;
 
   constructor(private dataService: DataService) { }
-
+  onSubmit() {
+    console.log(this.addNoteForm.value)
+    const newNote = new Note(this.addNoteForm.value['title'], this.addNoteForm.value['body'])
+    this.addNoteForm.reset();
+    this.dataService.addNote(newNote);
+  }
   ngOnInit() {
     this.notes = this.dataService.notes;
+    this.initForm();
   }
 
-
+  private initForm() {
+    let title = '';
+    let body = '';
+    this.addNoteForm = new FormGroup({
+      title: new FormControl(title),
+      body: new FormControl(body)
+    })
+  }
 
 }
